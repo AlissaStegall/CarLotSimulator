0afd2be02558704e2bc4b955543ce2a57007c6b6
AlissaStegall <alissaistegall@gmail.com>
2023-05-08 17:49:27 -0700

    created 2 classes, properties/objects

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace CarLotSimulator
+{
+	public class Car
+	{
+		public Car()
+		{
+
+		}
+		public Car(string make,
+				string model,
+				int year,
+				string enginenoise,
+				string honknoise,
+				bool isdrivable)
+		{
+			Make = make;
+			Model = model;
+			Year = year;
+			EngineNoise = enginenoise;
+			HonkNoise = honknoise;
+			IsDrivable = isdrivable;
+		}
+
+
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public int Year { get; set; }
+		public string EngineNoise { get; set; }
+		public string HonkNoise { get; set; }
+		public bool IsDrivable { get; set; }
+
+		public void MakeEngineNoise(string engineNoise)
+		{
+			Console.WriteLine(engineNoise);
+		}
+		public void MakeHonkNoise(string honkNoise)
+		{
+			Console.WriteLine(honkNoise);
+		}
+	}
+}
+
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -28,6 +28,48 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+            var lot = new CarLot();
+
+            var car1 = new Car();
+            car1.Make = "Hyundai";
+            car1.Model = "Kona";
+            car1.Year = 2022;
+            car1.IsDrivable = true;
+            car1.EngineNoise = "vroom vroom";
+            car1.HonkNoise = "beep beep";
+
+            car1.MakeEngineNoise(car1.EngineNoise);
+            car1.MakeHonkNoise(car1.HonkNoise);
+
+            lot.Cars.Add(car1);
+
+            var car2 = new Car()
+            {
+                Make = "Honda",
+                Model = "Civic",
+                Year =2021,
+                EngineNoise = "Vroooooom",
+                HonkNoise = "Beeeep",
+                IsDrivable = true
+            };
+
+            car2.MakeEngineNoise(car2.EngineNoise);
+            car2.MakeHonkNoise(car2.HonkNoise);
+
+            lot.Cars.Add(car2);
+
+            var car3 = new Car("Chevy", "Nova", 1974, "bvrooom", "BEP", true);
+
+            car3.MakeEngineNoise(car3.EngineNoise);
+            car3.MakeHonkNoise(car3.HonkNoise);
+
+            lot.Cars.Add(car3);
+
+            foreach(var car in lot.Cars)
+            {
+                Console.WriteLine($"Year: {car.Year}, Make: {car.Make}, Model: {car.Model}");
+            }
         }
     }
 }
